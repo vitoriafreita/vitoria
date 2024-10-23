b5d1e19d68ecbba23039fe6066cb402697660289
vitoriafreita <131983556+vitoriafreita@users.noreply.github.com>
2024-10-23 08:41:37 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,5 +1,4 @@
-const url = 'https://raw.githubusercontent.com/vitoriafreita
-/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/vitoriafreita/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
